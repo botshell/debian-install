056333fa918e3ab974901f9657a8f10001d88d93
botshell <kxktb@outlook.com>
2026-03-28 17:18:27 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,14 +6,14 @@ if [ "$EUID" -ne 0 ]; then
 	echo "Run with sudo or as root. You can run the code below instead..."
 	echo
   	if [[ "${1-}" =~ ^([Yy][Ee][Ss]|[Yy])$ ]]; then
-		echo 'sudo bash -c '\''bash <(wget -qO- https://raw.githubusercontent.com/driverdrift/debian-install/main/install.sh) "$0"'\'' y'
+		echo 'sudo bash -c '\''bash <(wget -qO- https://raw.githubusercontent.com/botshell/debian-install/main/install.sh) "$0"'\'' y'
 	else
-		echo 'sudo bash -c "$(wget -qO- https://raw.githubusercontent.com/driverdrift/debian-install/main/install.sh)"'
+		echo 'sudo bash -c "$(wget -qO- https://raw.githubusercontent.com/botshell/debian-install/main/install.sh)"'
 	fi
 	exit 1
 fi
 
-REPO_URL="https://github.com/driverdrift/debian-install/archive/main.tar.gz"
+REPO_URL="https://github.com/botshell/debian-install/archive/main.tar.gz"
 WORKDIR="/tmp/debian-install"
 
 skip_confirm=${1:-false}
