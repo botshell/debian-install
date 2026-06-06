ccf1b522ce8d26cd91260ffb72b86f812800b8a3
botshell <kxktb@outlook.com>
2026-06-06 21:51:02 +0800

    Create set_time_zone.sh

---
+++ initialize_system/set_time_zone.sh
@@ -0,0 +1,19 @@
+if [ -n "$TZ_WIN" ]; then
+  HOURS=$(echo "$TZ_WIN" | cut -d':' -f1)
+  
+  # 使用 Bash 的算术扩展 $((...))需要在外面套上双括号，并对系统强制指定十进制解析
+  # 否则凡是以 0 开头的数字，系统都会默认它是“八进制”（Octal）数！
+  HOURS_CLEAN=$((10#$(echo "$HOURS" | sed 's/[-+]//')))
+
+  if [ "$HOURS_CLEAN" -eq 0 ]; then
+    MY_TZ="Etc/UTC"
+  elif [[ "$HOURS" == -* ]]; then
+    MY_TZ="Etc/UTC+${HOURS_CLEAN}"
+  else
+    MY_TZ="Etc/UTC-${HOURS_CLEAN}"
+  fi
+
+  # export 的真正威力不是向上改变父进程，而是向下影响它自己的子进程。
+  # source 命令则是在当前进程中读入并执行这个脚本
+  export TZ="$MY_TZ"
+fi
